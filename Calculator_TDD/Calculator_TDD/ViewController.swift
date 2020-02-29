a1362c63c0a1c91c6761a6dca55ff743b02c1214
Toru_Nakandakari <arusu0629@gmail.com>
2020-02-29 16:25:28 +0900

    UI配置

--- Calculator_TDD/Calculator_TDD/ViewController.swift
+++ Calculator_TDD/Calculator_TDD/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    /* 数字 */
+    @IBAction func pushedZero(_ sender: Any) {
+    }
+    @IBAction func pushedOne(_ sender: Any) {
+    }
+    @IBAction func pushedTwo(_ sender: Any) {
+    }
+    @IBAction func pushedThree(_ sender: Any) {
+    }
+    @IBAction func pushedFour(_ sender: Any) {
+    }
+    @IBAction func pushedFive(_ sender: Any) {
+    }
+    @IBAction func pushedSix(_ sender: Any) {
+    }
+    @IBAction func pushedSeven(_ sender: Any) {
+    }
+    @IBAction func pushedEight(_ sender: Any) {
+    }
+    @IBAction func pushedNine(_ sender: Any) {
+    }
+    
+    /* 小数点(.) */
+    @IBAction func pushedDot(_ sender: Any) {
+    }
+    
+    /* イコールと四則演算 */
+    @IBAction func pushedEqual(_ sender: Any) {
+    }
+    @IBAction func pushedAddition(_ sender: Any) {
+    }
+    @IBAction func pushedSubtraction(_ sender: Any) {
+    }
+    @IBAction func pushedMultiplication(_ sender: Any) {
+    }
+    @IBAction func pushedDivision(_ sender: Any) {
+    }
+    
+    /* その他 (AC, ±, %) */
+    @IBAction func pushedAllClear(_ sender: Any) {
+    }
+    @IBAction func pushedPlusMinus(_ sender: Any) {
+    }
+    @IBAction func pushedPercent(_ sender: Any) {
+    }
 }
-
